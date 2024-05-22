50fa01966a67931156920a0120c3b0bc337e5614
DW-J <jdawoon1993@gmail.com>
2024-05-22 17:25:58 +0900

    Add files via upload

---
+++ May22_1_IsA/src/Dog.java
@@ -0,0 +1,40 @@
+// A is a B : 상속관계
+//   => 개는 동물 '이다'
+public class Dog extends Animal { // Animal의 기능을 물려받아옴
+								// => Animal 안에 Dog가 '상속'되어있다
+	// 상속 : Inheritance
+	// 확장 : extends 
+	
+	String inhabit;
+	
+	public Dog() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Dog(String sort, int age, String inhabit) {
+		super(sort, age); // Animal 의 생성자 오버로딩
+		this.inhabit = inhabit;
+	}
+
+	// @XXX : Annotation => 어떤 기능을 자동으로 연결해준다
+	@Override 	// 상속받아온 method의 기능을 바꾸는 것
+	public void printInfo() {
+		// override를 사용하려면...
+		// 	상속받은 Class에서의 method명과 같아야!!!
+		super.printInfo(); // 강아지 클래스 기준 상위클래스.printInfo(); 
+		System.out.println(inhabit);
+	}
+	
+	// VS
+	// Overloading
+	// 		메소드를 만들 때 같은 기능이라면 메소드명을 같게 !
+	//		파라미터의 자료형이 다르거나, 
+	//		혹은 파라미터의 갯수가 달라야 함.
+	
+	
+}
+
+
+
+
+
