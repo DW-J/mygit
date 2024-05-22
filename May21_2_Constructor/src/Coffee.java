6cb21543e5076bf834d25637fb8a5222574c6904
DW-J <jdawoon1993@gmail.com>
2024-05-22 09:15:15 +0900

    Add files via upload

---
+++ May21_2_Constructor/src/Coffee.java
@@ -0,0 +1,25 @@
+
+public class Coffee {
+	String name;
+	int price;
+
+	public Coffee() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Coffee(String name, int price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public void printInfo() {
+		System.out.println(name);
+		System.out.println(price);
+		System.out.println("-------------------");
+	}
+	
+	
+	
+	
+}
