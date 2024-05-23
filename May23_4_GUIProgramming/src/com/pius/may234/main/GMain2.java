bad0ad25a54c2be94ea07515290e41ec4da6d3d9
DW-J <jdawoon1993@gmail.com>
2024-05-23 17:38:40 +0900

    Add files via upload

---
+++ May23_4_GUIProgramming/src/com/pius/may234/main/GMain2.java
@@ -0,0 +1,9 @@
+package com.pius.may234.main;
+
+import com.pius.may234.frame.NumberFrame;
+
+public class GMain2 {
+	public static void main(String[] args) {
+		NumberFrame nf = new NumberFrame();
+	}
+}
