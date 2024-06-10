f71f37221a064cce4caeb9d829db544f5550817d
DW-J <jdawoon1993@gmail.com>
2024-06-10 17:27:04 +0900

    Delete May23_4_GUIProgramming directory

--- May23_4_GUIProgramming/src/com/pius/may234/main/GMain1.java
+++
@@ -1,9 +0,0 @@
-package com.pius.may234.main;
-
-import com.pius.may234.frame.MakeFrame;
-
-public class GMain1 {
-	public static void main(String[] args) {
-		MakeFrame mf = new MakeFrame();
-	}
-}
