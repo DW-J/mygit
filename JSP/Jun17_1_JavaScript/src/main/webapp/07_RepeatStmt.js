5625b6b843c03030efab2c7d0843b496279ba8cd
DW-J <jdawoon1993@gmail.com>
2024-06-17 16:29:31 +0900

    Add files via upload

---
+++ JSP/Jun17_1_JavaScript/src/main/webapp/07_RepeatStmt.js
@@ -0,0 +1,54 @@
+/*alert("아무것도 하기 싫다.");
+alert("내일 오지 말까?");*/
+// 7. 반복문 (Java와 동일)
+//	동일한 작업을 여러번 반복할 때 사용
+
+// 1 ~ 10까지 출력(document.write())
+
+// for
+// while
+// do-while
+
+// for
+for(i = 1; i < 11; i++){
+	document.write(i + " ");
+document.write("<br>-----------------<br>");
+
+}
+// while
+let i1 = 1;
+while (i1 < 11) {
+	document.write(i1 + " ");
+	i1++;
+}
+document.write("<br>-----------------<br>");
+
+// do-while
+let i2 = 1;
+do {
+	document.write(i2 + " ");
+	i2++;
+} while (i2 < 11);
+document.write("<br>-----------------<br>");
+
+// 반복문 깨기
+//	break : 멈추고 해당 반복문을 빠져나옴
+//	continue : 해당 반복문을 멈추고 다음 반복으로 넘어가서 실행
+//	return : 비공식적
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
